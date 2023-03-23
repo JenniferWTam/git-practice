e44b245930f5f5ed527ce518d3cac406aef642d9
Edith <mxedithbaltazar@gmail.com>
2023-03-23 14:10:19 -0700

    jens error edith

--- function_d.py
+++ function_d.py
@@ -3,7 +3,8 @@ def max_value(numbers):
         in the list.
     """
     'Jen is goint to receive an Git Push Error! ;-P'
-    pass
+    ordered_list = sorted(numbers)
+    return ordered_list[-1]
 
 
 if __name__ == "__main__":
